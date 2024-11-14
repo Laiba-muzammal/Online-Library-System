a65fe7077ba0834129bbdb2c032dcadd85be94de
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2024-11-14 12:11:37 +0500

    Update online_library.py

--- online_library.py
+++ online_library.py
@@ -1,22 +1,3 @@
-# This program is for an online library system. The Library class has different features to manage books, borrowing, and wishlists.
-
-# Here’s what you can do with the Library:
-# 1. Display books: See all the books available in the library.
-# 2. Search for a book: Check if a specific book is in the library.
-# 3. Add a book: Add a new book to the library if it’s not already there.
-# 4. Remove a book: Take a book out of the library.
-# 5. Borrow a book: Take a book to read and add it to your borrowed list.
-# 6. Show borrowed books: See all the books you have borrowed.
-# 7. Return a book: Give back a book you borrowed.
-# 8. Show wishlist: See books you want to borrow later.
-# 9. Add to wishlist: Add a book from the library to your wishlist.
-# 10. Remove from wishlist: Take a book out of your wishlist.
-# 11. Exit: Leave the program.
-
-# You can interact with the library by choosing options from the main menu. 
-# Keep using the library until you decide to exit.
-
-
 class Library:
     def __init__(self):
         self.number_of_books=0
